4f046af66a70dde7948ba48e22dc78fba4b61780
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-04-26 22:15:40 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'ingredients/index'
+  get 'ingredients/show'
+  get 'ingredients/create'
+  get 'ingredients/update'
+  get 'ingredients/destroy'
   resources :users, only: [:index, :show, :create, :update] do
     member do
       patch 'change_password'
     end
   end
 
+  resources :recipes, only: [:index, :show, :create, :update, :destroy] do
+    resources :ingredients, only: [:create, :update, :destroy]
+    get 'show_stored', on: :collection  # Rota para exibir receitas armazenadas
+  end
+  
   post '/login', to: 'auth#login'
   scope '/auth' do 
     get '/' => 'auth#get_auth_user', as: :get_auth_user
