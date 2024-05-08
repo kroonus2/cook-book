0ab940b9f49ee43bdf96d56ddfa88c1b01ff7b97
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-05-08 16:10:15 -0300

    create user component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecipeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
