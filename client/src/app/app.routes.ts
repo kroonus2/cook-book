d20da6e14613ffe6044acf25ade259ed0c2c7e4e
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-05-11 18:42:59 -0300

    Update app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: RecipeComponent}
+
+];
