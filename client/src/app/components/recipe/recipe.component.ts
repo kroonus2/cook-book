b1cad765c00e893415fc067cd90dcddf125ad97d
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-05-11 18:43:32 -0300

    update recipes component

--- client/src/app/components/recipe/recipe.component.ts
+++ client/src/app/components/recipe/recipe.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
 import { RecipeDetail } from '../../interfaces/recipe-datail';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, ],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
@@ -22,7 +23,6 @@ export class RecipeComponent implements OnInit{
       next: (res: any) =>{
         this.recipes = res.recipes as RecipeDetail[];
         console.log("Recipes fetched successfully");
-        console.log(this.recipes);
       }, 
       error: (error) => {
         console.log("Error fetching recipes: ", error);
