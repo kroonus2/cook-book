8d24410a99bd45554cfdda0e44184ae0b1e13cb7
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-05-11 18:44:22 -0300

    create categories services

---
+++ client/src/app/services/categories.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+
+const BASE_URL = environment.apiUrl + 'categories/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CategoriesService {
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  getCategories(){
+    return this.http.get(BASE_URL);
+  }
+}
