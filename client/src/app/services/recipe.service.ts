1d4786018253635bc8bc498ed8e1d3bd742de80d
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-05-11 18:44:26 -0300

    Update recipe.service.ts

--- client/src/app/services/recipe.service.ts
+++ client/src/app/services/recipe.service.ts
@@ -2,9 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { RecipeDetail } from '../interfaces/recipe-datail';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 
-const BASE_URL = 'http://localhost:3000/recipes/';
+const BASE_URL = environment.apiUrl + 'recipes/';
 
 @Injectable({
   providedIn: 'root'
