37cf947943a41f05ea2173ba3c3307716b7f56dd
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-05-14 17:12:44 -0300

    Update app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { RecipeComponent } from './components/recipe/recipe.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 
 export const routes: Routes = [
-    {path: '', component: RecipeComponent}
-
+    {path: '', component: RecipeComponent},
+    {path: 'sign-up', component: SignupComponent},
+    {path: 'sign-in', component: SigninComponent},
 ];
