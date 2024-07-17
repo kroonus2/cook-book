24600e26612e8fa24fb8925f7a024c1e208ff800
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-07-17 13:59:13 -0300

    feat: change front page in routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { RecipeComponent } from './components/recipe/recipe.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { UserComponent } from './components/user/user.component';
+import { RecipePaginationComponent } from './components/recipe-pagination/recipe-pagination.component';
 
 
 export const routes: Routes = [
-    {path: '', component: RecipeComponent},
+    {path: '', component: RecipePaginationComponent},
     {path: 'sign-up', component: SignupComponent},
     {path: 'sign-in', component: SigninComponent},
     {path: 'user', component: UserComponent}
